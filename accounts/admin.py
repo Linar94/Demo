5546b7c8229fbb206098762d004f4eb1fc3bfa50
Linar <lintutus94@mail.ru>
2014-12-15 00:05:53 +0300

    #3707,#3706 fgfg12112 dfdfd

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 from accounts.models import *
 
 class TaskTrView(admin.ModelAdmin):
-    list_display = ('id', 'task_tr_name', 'username', 'password', 'server', 'user')
+    list_display = ('task_tr_name', 'username', 'password', 'server', 'user')
 
 class UserProfileView(admin.ModelAdmin):
     list_display = ('user', 'avatar')
+	list_display = ('user', 'avatar')
 
 admin.site.register(TaskTr, TaskTrView)
 admin.site.register(UserProfile, UserProfileView)
